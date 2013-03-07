71d1880aabe416e9bc556d04ba9e75fd173b5d1c
Hiroshi Kikuchi <kikuchy.h@gmail.com>
2013-03-07 11:56:15 +0900

    add tests for getting view initializing data

--- test/tests.js
+++ test/tests.js
@@ -15,11 +15,46 @@ test("check context", function(){
   ok(ctx instanceof SearchContext, "make SearchContext");
 });
 
-asyncTest("fetching template text", function(){
+asyncTest("fetching window template text", function(){
   $("#sandbox").append('<script id="window-template" type="text/x-handlebars-template" />');
   $("#window-template").load("../template/viewer.html.mustache",{}, function(){
     var cont = new PixivContextViewerController();
     ok(cont._fetchWindowTemplate(), "fetch window template text");
+    start();
+  });
+});
+
+asyncTest("fetching page template text", function(){
+  $("#sandbox").append('<script id="page-template" type="text/x-handlebars-template" />');
+  $("#page-template").load("../template/page.html.mustache",{}, function(){
+    var cont = new PixivContextViewerController();
+    ok(cont._fetchPageTemplate(), "fetch page template text");
+    start();
+  });
+});
+
+test("judge view should show window",function(){
+  var cont = new PixivContextViewerController();
+  ok(cont._shouldBeShow("http://www.pixiv.net/ranking.php?mode=daily"), "when previous page is ranking page");
+  ok(cont._shouldBeShow("http://www.pixiv.net/bookmark_new_illust.php"), "when previous page is recent illusts page");
+  ok(!cont._shouldBeShow("http://www.pixiv.net/member_illust.php"), "when previous page is member illust page");
+});
+
+asyncTest("get pages for initialiing view", function(){
+  var rankingurl = "http://www.pixiv.net/ranking.php?mode=rookie&p=2";
+  $.mockjax({
+    	url: rankingurl,
+	proxy: "proxy/ranking.html"
+  });
+  var cont = new PixivContextViewerController();
+  cont._shouldBeShow(rankingurl);
+  var dfd = cont._getInitialPages(1);
+  $.when(dfd).done(function(returned){
+    ok(typeof returned === "object", "retuened object");
+    ok(returned.pages.length, "object has Array named 'pages'");
+    ok(typeof returned.hasPrev === "boolean", "object has bool named 'hasPrev'");
+    ok(typeof returned.hasNext === "boolean", "object has bool named 'hasNext'");
+    ok(typeof returned.numOfPage === "number", "object has number named 'numOfPage'");
     start();
   });
 });
@@ -58,6 +93,34 @@ asyncTest("parseRankingPage", function(){
 	ok(pages[0].thumbnailUrl === "http://i1.pixiv.net/img47/img/bean1000/33953193_s.png?ctype=ranking", "page thumbnal url");
 	ok(pages[0].artistUrl=== "member.php?id=1618332&ref=rn-b-51-user", "page artist url");
 	start();
+  });
+});
+
+asyncTest("make pages", function(){
+  var rankingurl = "/ranking.php?mode=rookie&p=2"
+  var rc = new RankingContext(rankingurl);
+  $.mockjax({
+    	url: rankingurl,
+	proxy: "proxy/ranking.html"
+  });
+  $.when(rc._makePages(rankingurl)).done(function(pages){
+    ok(rc.hadBeenParsed, "already parsed");
+    equal(rc.pages.length, 50, "ranking page has 50 pages");
+    start();
+  });
+});
+
+asyncTest("get pages", function(){
+  var rankingurl = "/ranking.php?mode=rookie&p=2"
+  var rc = new RankingContext(rankingurl);
+  $.mockjax({
+    	url: rankingurl,
+	proxy: "proxy/ranking.html"
+  });
+  $.when(rc.getPages()).done(function(pages){
+    ok(rc.hadBeenParsed, "already parsed");
+    equal(rc.pages.length, 50, "ranking page has 50 pages");
+    start();
   });
 });
 
